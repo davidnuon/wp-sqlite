f156512c8762dbf93be9273539c1abb9c8e53996
David Nuon <davidnuongm@gmail.com>
2024-01-16 14:58:29 -0800

    Got flake to build

--- flake.nix
+++ flake.nix
@@ -19,11 +19,15 @@
           url = "https://wordpress.org/wordpress-6.4.2.tar.gz";
           sha256 = "1ma82pj72hkb1ajphvcda666w0mdg03ggks8m0k73b26cjimpmd8";
         };
+        wordpress_SQLitePlugin = fetchTarball {
+          url = "https://github.com/davidnuon/sqlite-database-integration/archive/refs/tags/2.1.6.tar.gz";
+          sha256 = "1d5v4kpbzv2pchv6plb0197zryhkilnb0zr4n0j7za9afcsgrfbw";
+        };
       in {
         packages = rec {
           wordpress-sqlite = with pkgs;
             stdenv.mkDerivation rec {
-              inherit wordpress;
+              inherit wordpress wordpress_SQLitePlugin;
 
               name = "wordpress-sqlite";
               buildInputs = [
@@ -31,13 +35,22 @@
               ];
 
               src = builtins.path {
-                path = ./.;
+                path = ./src;
                 name = "wordpress-sqlite-src";
               };
 
               buildPhase = ''
                 mkdir -p $out/build
-                cp -r $wordpress/* $out/build                
+                cp -r $wordpress/* $out/build
+                chmod -R 777 $out/build
+                cp -r $src/database $out/build/wp-content/database
+                cp -r $wordpress_SQLitePlugin $out/build/wp-content/plugins/sqlite-database-integration     
+                
+                chmod -R 777 $out/build
+                cp $out/build/wp-content/plugins/sqlite-database-integration/db.copy $out/build/wp-content/db.php
+
+            #    mv $out/build/wp-content/plugins/sqlite-database-integration/wp-includes/sqlite $out/build/wp-includes/sqlite
+            #    rm -rf $out/build/wp-content/plugins/sqlite-database-integration/wp-content
               '';
 
               installPhase = ''
