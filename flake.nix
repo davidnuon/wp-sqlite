de7e155c9453d25f65d1f337f019a7c89ecc3f31
David Nuon <davidnuongm@gmail.com>
2024-01-16 15:46:50 -0800

    Change flake to tarball inputs

--- flake.nix
+++ flake.nix
@@ -4,25 +4,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
+    wordpress = {
+      url = "https://wordpress.org/wordpress-6.4.2.tar.gz";
+      flake = false;
+    };
+    wordpress_SQLitePlugin = {
+      url = "https://github.com/davidnuon/sqlite-database-integration/archive/refs/tags/2.1.6.tar.gz";
+      flake = false;
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    wordpress,
+    wordpress_SQLitePlugin,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = nixpkgs.legacyPackages.${system};
-        wordpress = fetchTarball {
-          url = "https://wordpress.org/wordpress-6.4.2.tar.gz";
-          sha256 = "1ma82pj72hkb1ajphvcda666w0mdg03ggks8m0k73b26cjimpmd8";
-        };
-        wordpress_SQLitePlugin = fetchTarball {
-          url = "https://github.com/davidnuon/sqlite-database-integration/archive/refs/tags/2.1.6.tar.gz";
-          sha256 = "1d5v4kpbzv2pchv6plb0197zryhkilnb0zr4n0j7za9afcsgrfbw";
-        };
       in {
         packages = rec {
           wordpress-sqlite = with pkgs;
